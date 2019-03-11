48f389694517092302c41260f5c8f7f8f5cad1c1
Xinyi <xchen92@u.rochester.edu>
2019-03-11 10:46:15 +0800

    ;(

--- Project2/src/computerPlayer.java
+++ Project2/src/computerPlayer.java
@@ -6,94 +6,71 @@ public class computerPlayer extends TTYGolf{
 	public static void PlayAtGenesee() {
 		boolean x = true;
 		int i; //holeNum
+		int rI; //holeNum for robot
 		int j = 0;
+		int rJ = 0;
 		Random randGen = new Random();
 		while(x) {
 			int shotCount = 0;
 			int rshotCount =0;
 			GeneseePark gPark = new GeneseePark();
 			System.out.println("");
-			System.out.println("You are playing the Genesee Valley Park North Course");
+			System.out.println("You are playing the Genesee Valley Park North Course with a robot");
 			String[] GTee = gPark.getGParkTee();
 			int[] GYard = gPark.getGParkDistance();
 			int[] GPar = gPark.getGParkPar();
-			for(i=0;i<18;++i) {
-				shotCount = 0;//number of the shot for each hole
-				rshotCount=0;
-				j = 0;
-				int totalDistance = 0;
+			
+			//for robot loops
+			System.out.println("The robot starts first");
+			for(rI=0;rI<18;++rI) {
+				rshotCount = 0;//number of the shot for each hole
+				rJ = 0;
 				int rtotalDistance =0;
-				totalDistance = GYard[i];
-				rtotalDistance = GYard[i];
-				System.out.println("You are at the " + GTee[i]+" tee. " + GYard[i]+" yard, Par "+GPar[i]);
-				while(j==0) {
-					int distance = hitTheBall();
-					int rDistance = randGen.nextInt(100)-50;
+				rtotalDistance = GYard[rI];
+				System.out.println("The robot if at the " + GTee[rI]+" tee. " + GYard[rI]+" yard, Par "+GPar[rI]);
+				while(rJ==0) {
 				
-					System.out.println(GTee[shotCount] + " shot.");
+					int rDistance = randGen.nextInt(50)+200;//+distance
+				
+					System.out.println("robot's "+ GTee[shotCount] + " shot.");
 			
 					//both hit
-					if(Math.abs(totalDistance - distance)>=60 && Math.abs(rtotalDistance - rDistance)>=60) {
-						System.out.println("you hit the ball "+distance+" yards, nice!");
-						System.out.println("You are now "+Math.abs(totalDistance-distance)+" yards away from the hole");
+					if( Math.abs(rtotalDistance - rDistance)>=60) {
 						System.out.println("The robot hits the ball "+rDistance+" yards");
 						System.out.println("The robot is now "+Math.abs(rtotalDistance-rDistance)+" yards away from the hole");
-						
-						totalDistance = Math.abs(totalDistance - distance);
-						rtotalDistance = Math.abs(totalDistance - rDistance);
+							rtotalDistance = Math.abs(rtotalDistance - rDistance);
 						//robot..
-						shotCount+=1;
 						rshotCount+=1;
 					}
-					//player hits, robot on green
-					else if(Math.abs(totalDistance - distance)>=60 && Math.abs(rtotalDistance - rDistance)<60) {
-						System.out.println("you hit the ball "+distance+" yards, nice!");
-						System.out.println("You are now "+Math.abs(totalDistance-distance)+" yards away from the hole");
-						System.out.println("The robot hits the ball "+rDistance+" yards");
-						System.out.println("The robot is now on the Green "+Math.abs(rtotalDistance-rDistance)+" yards away from the hole");
-						
-						totalDistance = Math.abs(totalDistance - distance);
-						rtotalDistance = Math.abs(totalDistance - rDistance);
-						//robot..
-						shotCount+=1;
-					}
-					//player on green, robot hits
-					else if(Math.abs(totalDistance - distance)<60 && Math.abs(rtotalDistance - rDistance)>=60) {
-						System.out.println("You are on the green!");
-						System.out.println("You are now "+Math.abs(totalDistance - distance)+" yards away from the hole");
-						double finalDistance = Math.abs(totalDistance - distance);
+					
+					if(Math.abs(rtotalDistance - rDistance) <= 60) {
+						double rfinalDistance = Math.abs(rtotalDistance - rDistance);
 						while(j==0) {
-							double putDistance = Putting();
-							if(Math.abs(finalDistance-putDistance)<=1) {
+							double rputDistance = 0;/*FIXME*/
+							if(Math.abs(rfinalDistance-rputDistance)<=1) {
 								System.out.println(GTee[shotCount] + " shot.");
-								System.out.println("Great! You made it in "+(shotCount+2)+" shots!");
-								if (GPar[i]-shotCount-2 ==1 ) {//birdie
-									System.out.println("Damn! You made a Birdie!");	
-									}else if (GPar[i]==shotCount+2) {
-										System.out.println("You made par on this hole");	
-									}else if (GPar[i]==shotCount+1) {
-										System.out.println("Eh, Bogey on this hole :(");
-									}else if (GPar[i]<shotCount+1) {
-										System.out.println("Eww, over par!");
+								System.out.println("The robot made it in "+(shotCount+2)+" shots!");
+								if (GPar[rI]-shotCount-2 ==1 ) {//birdie
+									System.out.println("Damn! The robot made a Birdie!");	
+									}else if (GPar[rI]==shotCount+2) {
+										System.out.println("The robot made par on this hole");	
+									}else if (GPar[rI]==shotCount+1) {
+										System.out.println("The robot Bogey on this hole :(");
+									}else if (GPar[rI]<shotCount+1) {
+										System.out.println("The robot over par!");
 									}
 								System.out.println("");
-								j=1;//next hole :)
-								
-						System.out.println("The robot hits the ball "+rDistance+" yards");
-						System.out.println("The robot is "+Math.abs(rtotalDistance-rDistance)+" yards away from the hole");
-						
-						totalDistance = Math.abs(totalDistance - distance);
-						rtotalDistance = Math.abs(totalDistance - rDistance);
-						//robot..
-						shotCount+=1;
+								rJ=1;//next hole :)
+						     	rtotalDistance = Math.abs(rtotalDistance - rDistance);
+						     	rshotCount+=1;
 					}
 				
 							else {
 								System.out.println(GTee[shotCount] + " shot.");
-								System.out.println("you hit the ball "+putDistance+" yards, nice!");
-								System.out.println("You are now "+Math.abs(finalDistance-putDistance)+" yards away from the hole");
-								finalDistance = Math.abs(finalDistance - putDistance);
-								shotCount+=1;
+								System.out.println("The robot hit the ball "+rputDistance+" yards, nice!");
+								System.out.println("The robot is now"+Math.abs(rfinalDistance-rputDistance)+" yards away from the hole");
+								rfinalDistance = Math.abs(rfinalDistance - rputDistance);
+								rshotCount+=1;
 							}		
 						}
 				}
